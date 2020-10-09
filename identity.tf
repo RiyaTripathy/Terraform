ec32fd8690a8915626a59ecfe904b5230e9db34f
RiyaTripathy <45755142+RiyaTripathy@users.noreply.github.com>
2020-10-09 21:06:57 +0530

    Update identity.tf

--- identity.tf
+++ identity.tf
@@ -7,11 +7,9 @@ provider "okta" {
     base_url = var.base_url
     api_token = var.api_token
 }
-resource "okta_user_schema" "example" {
-  index       = "test"
-  title       = "test"
+resource "okta_user_base_schema" "example" {
+  index       = "customPropertyName"
+  title       = "customPropertyName"
   type        = "string"
-  description = "My custom property name"
   master      = "OKTA"
-  scope       = "SELF"
-    }
+}
