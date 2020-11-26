0127a565752b6bf0596c45e7361443c31d54a281
RiyaTripathy <45755142+RiyaTripathy@users.noreply.github.com>
2020-11-26 20:32:28 +0530

    Update identity.tf

--- identity.tf
+++ identity.tf
@@ -7,13 +7,21 @@ provider "okta" {
     base_url = var.base_url
     api_token = var.api_token
   }
- data "okta_app" "app_test" {
-  label = "TestLoginApp"
+resource "okta_app_oauth" "example" {
+  label                      = "TestLoginApp"
+  type                       = "browser"
+  grant_types                = ["authorization_code","implicit"]
+  redirect_uris              = ["https://benchmark.dev.performanceroundtables.com/login/callback"]
+  post_logout_redirect_uris  = ["https://benchmark.dev.performanceroundtables.com"]
+  response_types             = ["code","token","id_token" ]
+     }
+
+  resource "okta_group" "FBImember_group" {
+  name        = "FBI Member"
+  description = "FBI Member with role code EXT1"
 }
-data "okta_group" "FBImember_group" {
-  name = "FBI Member"
-}
-resource "okta_app_group_assignment" "test" {
-  app_id   = data.okta_app.app_test.id
-  group_id = data.okta_group.FBImember_group.id
+
+resource "okta_group" "FBImemberadmin_group" {
+  name        = "FBI Member Admin"
+  description = "FBI Member Admin with role code EXT2"
 }
