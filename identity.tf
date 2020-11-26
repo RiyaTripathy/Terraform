db72cfa08429be6d516089beebd6059a2f37fe5a
RiyaTripathy <45755142+RiyaTripathy@users.noreply.github.com>
2020-11-26 20:39:43 +0530

    Update identity.tf

--- identity.tf
+++ identity.tf
@@ -36,5 +36,9 @@ data "okta_group" "FBImemberadmin_group" {
 }
 resource "okta_app_group_assignment" "test" {
   app_id   = data.okta_app.app_test.id
-  group_id = [data.okta_group.FBImember_group.id,data.okta_group.FBImemberadmin_group.id]
+  group_id = data.okta_group.FBImember_group.iddata.okta_group.FBImemberadmin_group.id
+}
+resource "okta_app_group_assignment" "test1" {
+  app_id   = data.okta_app.app_test.id
+  group_id = data.okta_group.FBImemberadmin_group.id
 }
