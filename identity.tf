bb427a5aa9135a928c793ccdecaa6d6162874c86
RiyaTripathy <45755142+RiyaTripathy@users.noreply.github.com>
2020-11-26 12:50:31 +0530

    Update identity.tf

--- identity.tf
+++ identity.tf
@@ -7,6 +7,15 @@ provider "okta" {
     base_url = var.base_url
     api_token = var.api_token
   }
- data "okta_group" "example" {
-  name = "Example1"
-}
+ resource "okta_app_oauth" "example" {
+  label                      = "TestLoginApp"
+  type                       = "single page app"
+  grant_types                = ["authorization_code","implicit"]
+  redirect_uris              = ["https://benchmark.dev.performanceroundtables.com/login/callback"]
+  post_logout_redirect_uris  = ["https://benchmark.dev.performanceroundtables.com"]
+  response_types             = ["code","token","id_token" ]
+  consent_method             = ["REQUIRED"],
+  issuer_mode                = ["CUSTOM_URL"]
+  initiate_login_uri         = ["https://benchmark.dev.performanceroundtables.com/login"]
+  issuer_mode                = ["CUSTOM_URL"]
+     }
