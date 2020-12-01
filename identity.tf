d665ddaf3d01831cac1d5304dd87e963718adbf6
RiyaTripathy <45755142+RiyaTripathy@users.noreply.github.com>
2020-12-01 17:58:06 +0530

    Update identity.tf

--- identity.tf
+++ identity.tf
@@ -7,6 +7,15 @@ provider "okta" {
     base_url = var.base_url
     api_token = var.api_token
   }
+resource "okta_template_email" "example" {
+  type = "email.forgotPassword"
+
+  translations {
+    language = "en"
+    subject  = "Stuff"
+    template = "Hi $${user.firstName},<br/><br/>Blah blah $${resetPasswordLink}"
+  }
+
 data "okta_group" "FBImember_group" {
   name = "FBI Member"
 }
