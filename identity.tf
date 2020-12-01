063d598115e25d4b746c3dd9717fc9dfaaf6441a
RiyaTripathy <45755142+RiyaTripathy@users.noreply.github.com>
2020-12-01 14:13:51 +0530

    Update identity.tf

--- identity.tf
+++ identity.tf
@@ -7,67 +7,11 @@ provider "okta" {
     base_url = var.base_url
     api_token = var.api_token
   }
-resource "okta_app_oauth" "example" {
-  label                      = "TestLoginApp"
-  type                       = "browser"
-  grant_types                = ["authorization_code","implicit"]
-  redirect_uris              = ["https://benchmark.dev.performanceroundtables.com/login/callback"]
-  post_logout_redirect_uris  = ["https://benchmark.dev.performanceroundtables.com"]
-  response_types             = ["code","token","id_token" ]
-     }
+resource "okta_template_email" "example" {
+  type = "email.forgotPassword"
 
-  resource "okta_group" "FBImember_group" {
-  name        = "FBI Member"
-  description = "FBI Member with role code EXT1"
-}
-
-resource "okta_group" "FBImemberadmin_group" {
-  name        = "FBI Member Admin"
-  description = "FBI Member Admin with role code EXT2"
-}
-data "okta_app" "app_test" {
-  label = "TestLoginApp"
-}
-data "okta_group" "FBImember_group" {
-  name = "FBI Member"
-}
-data "okta_group" "FBImemberadmin_group" {
-  name = "FBI Member Admin"
-}
-resource "okta_app_group_assignment" "test" {
-  app_id   = data.okta_app.app_test.id
-  group_id = data.okta_group.FBImember_group.id
-}
-resource "okta_app_group_assignment" "test1" {
-  app_id   = data.okta_app.app_test.id
-  group_id = data.okta_group.FBImemberadmin_group.id
-}
-data "okta_auth_server" "authserver_test" {
-  name = "default"
-}
-resource "okta_auth_server_claim" "div_claim" {
-  auth_server_id = data.okta_auth_server.authserver_test.id
-  name           = "division_code"
-  value_type     = "EXPRESSION"
-  value          = "user.divisions"
-  claim_type     = "RESOURCE"
-}
-resource "okta_auth_server_claim" "role_claim" {
-  auth_server_id = data.okta_auth_server.authserver_test.id
-  name           = "role"
-  value_type     = "EXPRESSION"
-  value          = "user.role_code"
-  claim_type     = "RESOURCE"
-}
-resource "okta_auth_server_claim" "company_claim" {
-  auth_server_id = data.okta_auth_server.authserver_test.id
-  name           = "company_code"
-  value_type     = "EXPRESSION"
-  value          = "user.company"
-  claim_type     = "RESOURCE"
-}
-resource "okta_trusted_origin" "trusted_test" {
-  name   = "FBI Benchmark URL"
-  origin = "https://example.com"
-  scopes = ["CORS","REDIRECT"]
-}
+  translations {
+    language = "en"
+    subject  = "Riya Password Reset"
+    template = "Hi $${user.firstName},<br/><br/>Blah blah $${resetPasswordLink}"
+  }
